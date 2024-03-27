3bb5e5763209c1c0c4eab157e963120ba8f8b009
serdarsen <serdarsendev@gmail.com>
2024-03-27 08:02:30 +0300

    style: format and lint codebase with prettier and eslint.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from "@angular/core";
-import { BrowserModule, provideClientHydration } from "@angular/platform-browser";
+import {
+  BrowserModule,
+  provideClientHydration
+} from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
